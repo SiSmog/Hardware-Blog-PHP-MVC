14803e65fb2cffda7464fe4a57aafb84ec886ebe
SiSmog <majdirekik60@gmail.com>
2022-12-10 19:11:55 +0100

    updated

--- Controllers/ArticleController.php
+++ Controllers/ArticleController.php
@@ -1,14 +1,21 @@
 <?php
-require("Models/ArticleModel.php");
-require("Models/CommentModel.php");
 class ArticleController
 {
     public static function Fetch($id)
-    {
+    {   require_once("Models/ArticleModel.php");
+        require_once("Models/CommentModel.php");
         $article = new ArticleModel();
         $Article = $article->FetchById($id);
         $comment = new CommentModel();
         $CommentsByArticle = $comment->FetchByArticle($id);
         include("Views/article.php");
     }
+    public static function Detail($id)
+    {   require_once("Models/ArticleModel.php");
+        $article = new ArticleModel();
+        $Article = $article->FetchById($id);
+        include("Views/detail.php");
+
+    }
+
 }
