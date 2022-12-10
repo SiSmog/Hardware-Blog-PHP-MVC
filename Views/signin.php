2bae15031209fe9eb8ffa8e1571786f906042d89
SiSmog <majdirekik60@gmail.com>
2022-12-10 19:01:26 +0100

    signup fix

--- Views/signin.php
+++ Views/signin.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/login.css"/>
+    <link rel="stylesheet" href="/css/sign.css"/>
 </head>
 <body class="align">
 
@@ -33,9 +33,7 @@
       
           </form>
       
-          <p class="text--center">Not a member? <a href="/Signup">Sign up now</a> <svg class="icon">
-              <use xlink:href="#icon-arrow-right"></use>
-            </svg></p>
+          <p class="text--center">Not a member? <a href="/Signup">Sign up now</a>
       
         </div>
       
@@ -49,6 +47,7 @@
           <symbol id="icon-user" viewBox="0 0 1792 1792">
             <path d="M1600 1405q0 120-73 189.5t-194 69.5H459q-121 0-194-69.5T192 1405q0-53 3.5-103.5t14-109T236 1084t43-97.5 62-81 85.5-53.5T538 832q9 0 42 21.5t74.5 48 108 48T896 971t133.5-21.5 108-48 74.5-48 42-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-320-893q0 159-112.5 271.5T896 896 624.5 783.5 512 512t112.5-271.5T896 128t271.5 112.5T1280 512z" />
           </symbol>
+          
         </svg>
       
 </body>
