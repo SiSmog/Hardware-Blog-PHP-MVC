7bf27d36ea1bf430fc1a879c7c3bae099e1dc9a3
SiSmog <majdirekik60@gmail.com>
2022-12-11 14:22:03 +0100

    execute close link

--- Models/execute.php
+++ Models/execute.php
@@ -8,19 +8,19 @@
         if ($params==null) {
             if ($res = mysqli_query($link, $sql)) {
                 if (mysqli_num_rows($res) > 0) {
+                    mysqli_close($link);
                     return $res;
                 }
             } else {
+                mysqli_close($link);
                 return false;
             }
         } else {
             $stmt = $link->prepare($sql);
             $stmt->execute($params);
             $res = $stmt->get_result();
+            mysqli_close($link);
             return $res;
         }
-        
-        mysqli_close($link);
-
     }
 ?>
