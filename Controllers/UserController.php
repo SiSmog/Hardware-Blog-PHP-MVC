7d97379a989084d5c216050fab2353d2afcf3873
SiSmog <majdirekik60@gmail.com>
2022-12-11 14:07:03 +0100

    logout

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -4,31 +4,53 @@ class UserController{
         include("Views/signup.php");
     }
     public static function Register(){
-        require_once("Models/UserModel.php");
-        $user=new UserModel();
-        $email=$_POST["email"];
-        $username=$_POST["username"];
-        $password=$_POST["password"];
-        $params=[$username,$email,$password];
-        $user->Add($params);
-        header("location:/Home");
+        if(isset($_SESSION["username"])){
+            header("location:/Home");
+        }else{
+            require_once("Models/UserModel.php");
+            $user=new UserModel();
+            $email=$_POST["email"];
+            $username=$_POST["username"];
+            $password=$_POST["password"];
+            $params=[$username,$email,$password];
+            $user->Add($params);
+            $params=[$username,$password];
+            $valid=$user->Check($params);
+            if($row=mysqli_fetch_array($valid)){
+                $_SESSION["username"]=$username;
+                $_SESSION["userimage"]=$row["image"];
+                $_SESSION["userid"]=$row["id"];
+            }
+            header("location:/Home");
+        }
+        
     }
     public static function Signin(){
         include("Views/signin.php");
     }
     public static function Login(){
-        require_once("Models/UserModel.php");
-        $user=new UserModel();
-        $username=$_POST["username"];
-        $password=$_POST["password"];
-        $params=[$username,$password];
-        $valid=$user->Check($params);
-        if($row=mysqli_fetch_array($valid)){
-            $_SESSION["username"]=$username;
-            $_SESSION["userimage"]=$row["image"];
-            $_SESSION["userid"]=$row["id"];
+        if(isset($_SESSION["username"])){
+            header("location:/Home");
+        }else{
+            require_once("Models/UserModel.php");
+            $user=new UserModel();
+            $username=$_POST["username"];
+            $password=$_POST["password"];
+            $params=[$username,$password];
+            $valid=$user->Check($params);
+            if($row=mysqli_fetch_array($valid)){
+                $_SESSION["username"]=$username;
+                $_SESSION["userimage"]=$row["image"];
+                $_SESSION["userid"]=$row["id"];
+            }
+            header("location:/Home");
         }
-        header("location:/Home");
+        
     }
+    public static function Logout(){
+        $_SESSION["username"]=null;
+        $_SESSION["userimage"]=null;
+        $_SESSION["userid"]=null;
+    }
+
 }
-?>
